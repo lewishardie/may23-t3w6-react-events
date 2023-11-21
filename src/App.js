67e6ae2e015a571ff2eeecc7ff81797de138f1ec
Lewbagz <lewishardie@gmail.com>
2023-11-21 19:45:07 +1100

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// Page that lets us increase or decrease
+// how many Pokemon are on screen
+// App components needs to:
+//  - Keep track of how many Pokemon to show
+//  - Iterate over a single, DRY modular component
+//  - Button to increase Pokemon count
+//  - Button to decrease Pokemon count
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+  
+    // srart with at least one pokemon on screen
+    this.state = {
+      pokemonCount: 1
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Pokemon Page</h1>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
